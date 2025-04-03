0df6d02fa4a1d7aad932c4e987ad37d69640a555
kym8821 <louie9798@naver.com>
2025-04-03 18:47:53 +0900

    [fix] common 모듈 빌드 문제 해결 - jar.enabled=true 설정 - main 클래스 추가를 통해 main 클래스를 못찾는 문제 해결

---
+++ common/src/main/java/com/theme/CommonModuleApplication.java
@@ -0,0 +1,8 @@
+package com.theme;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonModuleApplication {
+    public static void main(String[] args) {}
+}

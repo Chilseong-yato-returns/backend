a764b1d247253568a2beaecd7018ad1b1d6cd3f9
kym8821 <louie9798@naver.com>
2026-09-06 09:53:23 +0900

    feat: [#160 - ] 테마 상세 조회 시 이미지별 대분류 추가 - ID 기반 테마 상세 조회 시 이미지별 대분류 함께 제공 Fixes: #160

--- src/main/java/com/komentum/theme/core/dto/ThemeDetailResponse.java
+++ src/main/java/com/komentum/theme/core/dto/ThemeDetailResponse.java
@@ -1,9 +1,12 @@
 package com.komentum.theme.core.dto;
 
+import com.komentum.designcomponent.domain.ComponentType;
 import com.komentum.designcomponent.domain.DesignComponent;
 import com.komentum.designcomponent.enums.PlatformScope;
 import com.komentum.designcomponent.enums.StyleCode;
 import com.komentum.designcomponent.enums.TypeCode;
+import com.komentum.designcomponent.enums.TypeCodeGroup;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,24 +19,45 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "테마 상세 조회 응답 DTO")
 public class ThemeDetailResponse {
+
+  @Schema(description = "테마 식별자")
+  private Integer themeComponentId;
+  @Schema(description = "테마 이름")
+  private String themeName;
+  @Schema(description = "typeCode별 이미지 정보")
+  Map<TypeCode, TypeCodeInfo> typeCodes;
+  @Schema(description = "styleCode별 색상 정보")
+  Map<StyleCode, StyleCodeInfo> styleCodes;
 
   @Getter
   @Setter
   @Builder
   @NoArgsConstructor
   @AllArgsConstructor
+  @Schema(description = "typeCode에 대한 색상 정보")
   public static class TypeCodeInfo {
 
+    @Schema(description = "디자인 에셋 ID")
     Integer designComponentId;
+    @Schema(description = "디자인 에셋 이미지 URL")
     String imageUrl;
+    @Schema(description = "호환되는 플랫폼 정보", example = "ANDROID | COMMON | IOS")
     PlatformScope platformScope;
+    @Schema(description = "이미지 대분류")
+    TypeCodeGroup typeCodeGroup;
+    @Schema(description = "이미지 대분류 이름")
+    String typeCodeGroupName;
 
-    public static TypeCodeInfo of(DesignComponent designComponent, PlatformScope platformScope) {
+    public static TypeCodeInfo of(DesignComponent designComponent, ComponentType componentType) {
+      TypeCode typeCode = componentType.getTypeCode();
       return TypeCodeInfo.builder()
           .designComponentId(designComponent.getDesignComponentId())
           .imageUrl(designComponent.getImageUrl())
-          .platformScope(platformScope)
+          .platformScope(componentType.getPlatformScope())
+          .typeCodeGroup(typeCode.getTypeCodeGroup())
+          .typeCodeGroupName(typeCode.getTypeCodeGroup().getDescription())
           .build();
     }
   }
@@ -57,9 +81,4 @@ public class ThemeDetailResponse {
           .build();
     }
   }
-
-  private Integer themeComponentId;
-  private String themeName;
-  Map<TypeCode, TypeCodeInfo> typeCodes;
-  Map<StyleCode, StyleCodeInfo> styleCodes;
 }
